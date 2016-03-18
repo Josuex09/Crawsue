a765b40da8d6bc183e17adf0cafb71da01dfcd4e
Josuex09 <josuex09@hotmail.es>
2016-03-18 16:28:18 -0500

    Update BotConfig.java

--- src/main/java/com/alsoftware/josuex09/crawlerbot/config/BotConfig.java
+++ src/main/java/com/alsoftware/josuex09/crawlerbot/config/BotConfig.java
@@ -5,7 +5,7 @@ package com.alsoftware.josuex09.crawlerbot.config;
  */
 public class BotConfig {
     
-    public static final String TOKEN = "183022252:AAEW75qQPGmuMRaQ3i368e0jCfUJD7aLVjk";
+    public static final String TOKEN = "TOKEN";
     public static final String USERNAME = "Josue001Bot";
     public static final String FILE_ERROR_MESSAGE = "Wrong URL, please try again with a valid url,remember this bot only accepts text files eg: html, pdf, txt";
     public static final String MSG_ERROR_MESSAGE = "You need to send a URL";
